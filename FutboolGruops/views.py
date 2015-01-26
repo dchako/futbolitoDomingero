9f2689865abe0896c9d4accefb6a64726aef93a2
dchako <manu_666_126@hotmail.com>
2015-01-26 00:32:49 -0300

    agragando 6 vistas!

--- FutboolGruops/views.py
+++ FutboolGruops/views.py
@@ -8,3 +8,15 @@ def home(request):
 
 def error(request):
 	return render_to_response('error.html',context_instance=RequestContext(request))
+
+def invitacion(request):
+	return render_to_response('invitacion.html',context_instance=RequestContext(request))
+
+def invitar(request):
+	return render_to_response('invitar.html',context_instance=RequestContext(request))
+
+def login(request):
+	return render_to_response('login.html',context_instance=RequestContext(request))
+
+def registrar(request):
+	return render_to_response('registrar.html',context_instance=RequestContext(request))
--- RunPlayers/urls.py
+++ RunPlayers/urls.py
@@ -6,6 +6,10 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'FutboolGruops.views.home', name='home'),
+    url(r'^invitacion$', 'FutboolGruops.views.invitacion', name='invitacion'),
+    url(r'^invitar$', 'FutboolGruops.views.invitar', name='invitar'),
+    url(r'^login$', 'FutboolGruops.views.login', name='login'),
+    url(r'^registrar$', 'FutboolGruops.views.registrar', name='registrar'),
     url(r'^error$', 'FutboolGruops.views.error', name='error'),
     # url(r'^blog/', include('blog.urls')),
     url('', include('social.apps.django_app.urls', namespace='social')),
