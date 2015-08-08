3e6611fe8ef715c5c16b1a30cdc252cd28a4de9f
dchako <dchako_623@hotmail.com>
2015-08-08 00:47:22 -0300

    js arreglando

---
+++ settings_local.py
@@ -0,0 +1,30 @@
+from RunPlayers.settings import *
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'FutboolGruops',
+    'social.apps.django_app.default',
+)
+# Database
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
