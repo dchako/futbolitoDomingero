60931c28b022e4334f6d4c8019a351e0535d164f
dchako <dchako_623@hotmail.com>
2016-05-17 22:25:57 -0300

    static field_2

--- RunPlayers/settings.py
+++ RunPlayers/settings.py
@@ -160,13 +160,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
-#PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
 #STATIC_ROOT = "staticfiles"
 #STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_ROOT= os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT= os.path.join(PROJECT_ROOT,'../staticfiles')
 STATICFILES_DIRS = (
-   os.path.join(BASE_DIR, 'FutboolGruops/static'),
+   os.path.join(PROJECT_ROOT, '../FutboolGruops/static'),
 #    os.path.join(PROJECT_ROOT, 'static'),
 )
 
