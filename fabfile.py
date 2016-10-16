47e2d211c4118f1ccdb9e7a69d3ec33ba03a724e
manuel <dchako_623@hotmail.com>
2016-10-16 17:49:39 -0300

    cambios en fabfile

--- fabfile.py
+++ fabfile.py
@@ -9,6 +9,7 @@ DEFAULT    = 'default'
 STATIC     = 'static'
 HEROKU     = 'heroku'
 PATH       = 'path'
+MIRROR     = 'mirror'
 
 
 STORAG = 'FILE_STORAGE'
@@ -29,8 +30,12 @@ MIRROR_PRODUCTION = MAIN_PRODUCTION
 MIRROR_STAGGING   = MAIN_PRODUCTION
 
 
-EXTERNAL_APPS = {DEFAULT    :{PRODUCTION: { HEROKU: 'runplayers',       STATIC: MAIN_PRODUCTION },
-                              },
+EXTERNAL_APPS = { DEFAULT     :{ PRODUCTION: { HEROKU: 'runplayers',       STATIC: MAIN_PRODUCTION },
+                                 MIRROR: True,
+                               },
+                 'runplayers' :{ PRODUCTION: { HEROKU: 'runplayers',      STATIC: MIRROR_PRODUCTION },
+                                 MIRROR: True,
+                               },
                 }
 
 APP         = EXTERNAL_APPS[DEFAULT][PRODUCTION]
@@ -38,6 +43,33 @@ HEROKU_APP  = EXTERNAL_APPS[DEFAULT][PRODUCTION][HEROKU]
 
 DUMP_FILE = 'dump.sql'
 
+
+@task
+def app(application):
+    """ 
+        Configure which remote application to use
+
+        f.e.    fab deploy:prod
+                fab deploy:prod-lets
+                fab deploy:prod-big
+                fab deploy:prod-abm
+    """
+    global APP
+    global VERSION
+    global HEROKU_APP
+
+    version = application.split('-')[0] if application and type(application) == str else ''
+    app     = application.split('-')[1] if len(application.split('-')) >= 2 else DEFAULT
+
+    if version not in [PRODUCTION, STAGGING]:
+        raise ValueError("Invalid version option: {app}. Only support prod and stg".format(app=version))
+
+    if app in EXTERNAL_APPS.keys():
+        APP        = app
+        VERSION    = version
+        HEROKU_APP = EXTERNAL_APPS[app][version][HEROKU]
+    else:
+        raise ValueError("Invalid APP name: {app}".format(app=app))
 
 
 def heroku_run(cmd, *args, **kwargs):
@@ -76,3 +108,31 @@ def bootstrap():
     loaddb()
 
     # Grab homepage images and store them in staticfiles directory
+
+@task
+def push(remote=None, branch='master', heroku_app=HEROKU_APP):
+    """Push the master branch to the remote application"""
+
+    if remote:
+        print "PUSH TO GITHUB \t Branch:{})".format(branch)
+    else:
+        print "PUSH TO HEROKU {} \t Branch:{}".format(heroku_app, branch)
+
+    if remote:
+        local('git push {remote} {branch}:{branch}'. \
+        format(remote=remote, branch=branch))
+    else:
+        local('git push -f git@heroku.com:{app}.git {branch}:master'. \
+        format(app=heroku_app, branch=branch))
+
+
+@task
+def deploy(application, branch='master'):
+    """Deploy the given remote application"""
+    app(application)
+
+    if PRODUCTION in application:
+        push(remote='origin', branch=branch) # Main repository
+
+    if EXTERNAL_APPS[APP][MIRROR]:
+        push(branch=branch, heroku_app=EXTERNAL_APPS[APP][VERSION][HEROKU])
